10982e70e6a2385d9d0b11d1519131bace858d0a
Nicholas Andrews <nicholas.andrews01@gmail.com>
2026-05-23 17:43:03 -0400

    adds vercel analytics.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
+import { inject } from '@vercel/analytics';
 
+
+inject();
 bootstrapApplication(AppComponent, appConfig).catch(err => console.error(err));
